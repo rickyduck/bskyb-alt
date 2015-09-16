8257dc29db4c1ef7fa25d809acb1cb651828a20d
Ricky Duckworth <ricky.duckworth@first-utility.com>
2015-09-16 09:10:29 +0100

    added tables, tests and more styling.

--- src/components/SkyStore.jsx
+++ src/components/SkyStore.jsx
@@ -2,6 +2,7 @@ import alt from '../libs/alt';
 import { RouteHandler, Link } from 'react-router';
 import React from 'react';
 import Paginator from 'react-pagify';
+import numberUtils from '../utils/numberUtils';
 
 import BillStore from '../stores/BillStore';
 
@@ -16,17 +17,7 @@ export default class SkyStore extends React.Component {
     constructor(props) {
         super(props);
         this.state = BillStore.getState();
-        this.state.pagination = {
-            rental: {
-                page: 0,
-                perPage: 10
-            },
-            buyAndKeep: {
-                page: 0,
-                perPage: 10
-            }
-        }
-        this.onSelect = this.onSelect.bind(this);
+
     }
     componentDidMount() {
         BillStore.listen(this.onChange);
@@ -37,25 +28,10 @@ export default class SkyStore extends React.Component {
     onChange(state) {
         this.setState(state);
     }
-    onSelect(page, type) {
-        let pagination = this.state.pagination || {};
-        pagination[type].page = page;
-        this.setState({
-            pagination: pagination
-        });
-    }
-    onPerPage(e) {
-        let pagination = this.state.pagination || {};
-        pagination.perPage = parseInt(event.target.value, 10);
-        this.setState({
-            pagination: pagination
-        });
-    }
+
     render() {
         const bill = this.state.bill;
         const skyStore = bill.skyStore;
-        const pagination = this.state.pagination;
-        const paginatedRental = Paginator.paginate(skyStore.rental, pagination.rental);
 
         return (<div className="col-md-12">
             <div className="row">
@@ -68,19 +44,18 @@ export default class SkyStore extends React.Component {
                 </aside>
                 <div className="col-md-8">
                     <h2>Subscriptions</h2>
+                    <h3>Total - {numberUtils.formatGBP(skyStore.total)}</h3>
                     <hr />
                     <h3>Rentals</h3>
                     <section className="recent-calls">
-                        <SkyStoreTable data={paginatedRental.data} />
-                        <div className='pagination'>
-                            <Paginator
-                                page={paginatedRental.page}
-                                pages={paginatedRental.amount}
-                                beginPages={3}
-                                endPages={3}
-                                onSelect={this.onSelect.bind(null, "rental")}>
-                            </Paginator>
-                        </div>
+                        <SkyStoreTable data={skyStore.rentals} />
+
+                    </section>
+                    <hr />
+                    <h3>Buy and Keep</h3>
+                    <section className="recent-calls">
+                        <SkyStoreTable data={skyStore.buyAndKeep} />
+
                     </section>
                 </div>
             </div>
